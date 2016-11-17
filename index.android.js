c1be23e5ac18d18e16b2011b6e562807ad3992a7
Sævar Berg <icedane@gmail.com>
2016-11-17 21:32:07 +0100

    Unbroke index.android.js

--- index.android.js
+++ index.android.js
@@ -6,65 +6,43 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  Image,
-  View,
-  ScrollView
+    AppRegistry,
+    StyleSheet,
+    Text,
+    Image,
+    View,
+    ScrollView
 } from 'react-native';
 
 export default class testproject extends Component {
-  render() {
-    var url = { uri: "http://www.imada.sdu.dk/~jogoe12/images/IMADA_FAGRAAD_LOGO2.png" }
-	// <Image style={{width: 100, height:100}} source={url}/>
-    return (
-      <ScrollView contentContainerStyle={styles.container}>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "blue"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View style={{height:50, width:50, backgroundColor: "red"}}></View>
-	<View>
-
-      </ScrollView>
-    );
-  }
+    render() {
+        var url = { uri: "http://www.imada.sdu.dk/~jogoe12/images/IMADA_FAGRAAD_LOGO2.png" }
+	      // <Image style={{width: 100, height:100}} source={url}/>
+        return (
+            <ScrollView contentContainerStyle={styles.container}>
+                <Text style={{fontSize: 50}}>Hello!</Text>
+            </ScrollView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 });
 
 AppRegistry.registerComponent('testproject', () => testproject);
