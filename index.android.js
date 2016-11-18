da87ca27d7f40831ce27b08302d6115ed1a637d4
Sævar Berg <icedane@gmail.com>
2016-11-18 01:57:14 +0100

    Added some example code.

--- index.android.js
+++ index.android.js
@@ -11,38 +11,99 @@ import {
     Text,
     Image,
     View,
-    ScrollView
+    ScrollView,
+    Button,
+    Dimensions,
+    TouchableHighlight
 } from 'react-native';
 
-export default class testproject extends Component {
+class MyButton extends Component {
+    static propTypes = {
+        text: React.PropTypes.string.isRequired,
+        onPress: React.PropTypes.func,
+    }
     render() {
-        var url = { uri: "http://www.imada.sdu.dk/~jogoe12/images/IMADA_FAGRAAD_LOGO2.png" }
-	      // <Image style={{width: 100, height:100}} source={url}/>
+        const formattedText = this.props.text.toUpperCase();
         return (
-            <ScrollView contentContainerStyle={styles.container}>
-                <Text style={{fontSize: 50}}>Hello!</Text>
-            </ScrollView>
+            <TouchableHighlight onPress={this.props.onPress} style={styles.buttonStyle}>
+                <Text style={styles.buttonTextStyle}>{formattedText}</Text>
+            </TouchableHighlight>
         );
     }
 }
 
+export default class testproject extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { textToShow: "Button 1" };
+    }
+
+    changeState(text) {
+        this.setState({textToShow: text});
+    }
+
+    render() {
+        var url = { uri: "http://www.imada.sdu.dk/~jogoe12/images/IMADA_FAGRAAD_LOGO2.png" }
+        return (
+            <View style={styles.mainContainer}>
+                <View style={{flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "grey"}}>
+                    <Text style={{fontSize: 50}}>
+                        {this.state.textToShow}
+                    </Text>
+                </View>
+                <View style={{flex: 1}}>
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.innerContainer}>
+                            <MyButton onPress={() => this.changeState("Button 1")} text="Button 1"/>
+                        </View>
+                        <View style={styles.innerContainer}>
+                            <MyButton onPress={() => this.changeState("Button 2")} text="Button 2"/>
+                        </View>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.innerContainer}>
+                            <MyButton onPress={() => this.changeState("Button 3")} text="Button 3"/>
+                        </View>
+                        <View style={styles.innerContainer}>
+                            <MyButton onPress={() => this.changeState("Button 4")} text="Button 4"/>
+                        </View>
+                    </View>
+                </View>
+            </View>
+        );
+    }
+}
+
+
 const styles = StyleSheet.create({
-    container: {
+    mainContainer: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    buttonStyle: {
+        elevation: 4,
+        backgroundColor: '#2196F3',
+        borderRadius: 6,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
     },
-    instructions: {
+    buttonTextStyle: {
         textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+        color: 'white',
+        padding: 8,
+        fontWeight: '500',
     },
+    innerContainer: {
+        flex: 1,
+        borderColor: "purple",
+        alignItems: "stretch",
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: "row",
+    }
 });
 
 AppRegistry.registerComponent('testproject', () => testproject);
