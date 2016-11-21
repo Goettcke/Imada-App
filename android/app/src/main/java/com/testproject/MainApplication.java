ec6a4b550d5a9c2a7eb943881146d33bb5705a59
Sævar Berg <icedane@gmail.com>
2016-11-21 13:32:35 +0100

    Added facebook SDK

--- android/app/src/main/java/com/testproject/MainApplication.java
+++ android/app/src/main/java/com/testproject/MainApplication.java
@@ -9,27 +9,51 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
+// Facebook SDK imports START <---
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
+// Facebook SDK imports END    --->
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    protected boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // If you want to use AppEventsLogger to log events.
+        // AppEventsLogger.activateApp(this);
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
-  }
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+            @Override
+            protected boolean getUseDeveloperSupport() {
+                return BuildConfig.DEBUG;
+            }
+
+            @Override
+            protected List<ReactPackage> getPackages() {
+                return Arrays.<ReactPackage>asList(
+                    new FBSDKPackage(mCallbackManager),
+                    new MainReactPackage()
+                );
+            }
+        };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
 }
