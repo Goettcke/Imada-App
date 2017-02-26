c0213ca54d6f9be4ded3c5e632ede2c05dc19f77
Skayze <christianmoeslund@msn.com>
2017-02-26 15:06:00 +0100

    Added facebook app-id and updated readme to show how to install aws-sdk-react-native

--- components/Challenges.js
+++ components/Challenges.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import AWSDynamoDB from 'aws-sdk-react-native-dynamodb';
 
-import styles from '../styles/home'
+import styles from '../styles/home';
 
 class Challenges extends Component {
     render(){
         return (
             <View style={styles.container}>
                 <Text>Challenges Screen</Text>
+
             </View>
         );
     }
