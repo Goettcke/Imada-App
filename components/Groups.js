e461c8b915da7e678d176d6707e09ddbd2927ba3
Jonatan Møller Gøttcke <jogoe12@student.sdu.dk>
2017-02-26 18:22:14 +0100

    changed menu and started AWS integration in the GROUPS component

--- components/Groups.js
+++ components/Groups.js
@@ -2,16 +2,56 @@
  * Created by jogoe12 on 2/20/2017.
  */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+
+import {
+    NativeModules,
+    NativeAppEventEmitter,
+    DeviceEventEmitter,
+    View,
+    Text,
+    TouchableOpacity,
+    Alert,
+    Button} from 'react-native';
+
+
+/*
+* Region */
+
+/*Most of the information on DynamoDB can be found on the
+* IntegrationTest file for Dynamo in DynamoDBTests.js  */
+
 
 import styles from '../styles/home'
+import {AWSDynamoDB} from 'aws-sdk-react-native-dynamodb';
+import {AWSCognitoCredentials} from 'aws-sdk-react-native-core'
+
+var tables = [];
+const cognitoRegion = "eu-west-1"; /*Ireland*/
+const identity_pool_id = "eu-west-1_H4tlEBzUU"; /*this is our actual ID*/
+const serviceRegion = "eu-central-1"; /*Frankfurt*/
+var hashTableName = "HashTableExample"
+var hashRangeTableName = "HashRangeTableExample";
+var shouldResolve = false;
+
+
+
+const onButtonPress =() => {
+    Alert.alert("Group created","Your Group has been created");
+}
 
 class Groups extends Component {
     render(){
         return (
             <View style={styles.container}>
-                <Text>Groups Screen</Text>
+                <Text>Groups View</Text>
+                <Button
+                    onPress={onButtonPress}
+                    title="Create Group"
+                    color = '#8000A8'
+                    accessibilityLabel="See an informative alert"
+                />
             </View>
+
         );
     }
 }
