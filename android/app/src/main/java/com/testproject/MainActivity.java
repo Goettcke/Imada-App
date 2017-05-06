e3c26631d3fdc766d36910a5fb0643d35988427f
Mads Grau Kristensen <madsgrau@gmail.com>
2017-05-06 21:38:31 +0200

    React upgrade and linking

--- android/app/src/main/java/com/testproject/MainActivity.java
+++ android/app/src/main/java/com/testproject/MainActivity.java
@@ -1,6 +1,5 @@
 package com.testproject;
 
-import android.content.Intent;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,11 +11,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "testproject";
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
     }
 }
